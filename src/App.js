d7b8b5e007fd6314af7c99cd69ca49cee5926c97
Nurbaeva Assol <nurbaevaassol@gmail>
2023-01-29 21:40:24 +0300

    catalog

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { Route, Routes } from 'react-router';
-import Layout from './Layout/Layout';
-import Deliver from './pages/deliver/deliver';
-import Home from './pages/Home/Home';
-import './sass/style.scss'
+import Catalog from "./pages/Catalog/Catalog";
+import './scss/style.scss'
+
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path='' element={<Layout/>}>
-        <Route path='/' element={<Home/>}/>
-        <Route path='deliver' element={<Deliver/>}/>
-      </Route>
-    </Routes>
+    <Catalog/>
     </>
   );
 }
---
+++ src/pages/Catalog/Catalog.jsx
@@ -0,0 +1,30 @@
+import React, { useContext, useEffect } from 'react'
+import CategoryTitle from '../../components/CategoryTitle'
+import { CustomContext } from '../../utils/Context'
+import CatalogRow from './CatalogRow/CatalogRow'
+import SideBar from './SideBar/SideBar'
+
+const Catalog = () => {
+    const {category, getProducts} = useContext(CustomContext)
+
+    useEffect(()=>{
+        getProducts()
+    }, [category])
+  return (
+    <selection>
+        <div className="container">
+            <CategoryTitle/>
+            <div className="catalog__content">
+                <div className="catalog__left">
+                    <SideBar/>
+                </div>
+                <div className="catalog__right">
+                    <CatalogRow/>
+                </div>
+            </div>
+        </div>
+    </selection>
+  )
+}
+
+export default Catalog
