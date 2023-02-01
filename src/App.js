596508ae585ed825a75faa99ef1d8fe919d783e6
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2023-02-01 17:35:16 +0600

    aidar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,26 @@
 import { Route, Routes } from 'react-router';
 import Layout from './Layout/Layout';
-import Contact from './pages/Contact/Contact';
 import Deliver from './pages/deliver/deliver';
-import ShowRoom from "./pages/showRoom/ShowRoom"
-import Home from './pages/Home/Home';
-import Like from './pages/Like/Like';
-import Zakaz from './pages/Zakaz/Zakaz';
+import Obmen from './pages/deliver/deliver copy';
+import Voz from './pages/deliver/deliver copy 2';
+import Pay from './pages/deliver/deliver copy 3';
+import Home from './pages/home';
 import './sass/style.scss'
-import Primerka from './pages/Primerka/Primerka';
-// import YandexMaps from './Components/YandexMaps/YandexMaps';
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path='' element={<Layout/>}>
-        <Route path='/' element={<Home/>}/>
-        <Route path='deliver' element={<Deliver/>}/>
-        <Route path='like' element={<Like/>}/>
-        <Route path='Contact' element={<Contact/>}/>
-        <Route path='showRoom' element={<ShowRoom/>}/>
-        <Route path='Primerka' element={<Primerka/>}/>
-        <Route path='zakaz' element={<Zakaz/>}/>
+      <Route path='/' element={<Layout/>}>
+        <Route path='Home' element={<Home/>}/>
+        <Route path='' element={<Deliver/>}/>
+        <Route path='Obmen' element={<Obmen/>}/>
+        <Route path='Voz' element={<Voz/>}/>
+        <Route path='Pay' element={<Pay/>}/>
       </Route>
     </Routes>
     </>
   );
-};
+}
 
 export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Deliver from './deliver/deliver copy'
+import Modal24 from './modal/modal24'
+
+const Home = () => {
+  return (
+    <main className='wrapper'>
+      {/* <Deliver/> */}
+      <Modal24/>
+    </main>
+  )
+}
+
+export default Home
